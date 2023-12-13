aa9d029b6d01bd3c105bbb8089792245fae97db4
AshwinGuptha <50212647+AshwinGuptha@users.noreply.github.com>
2023-12-13 13:18:54 -0500

    Add files via upload

---
+++ processing/fetch_content_with_docID.py
@@ -0,0 +1,30 @@
+
+
+
+def fetchContentWithDocID(docid):
+
+    docName = int(docid/5000)+1
+    i = docid%5000
+
+    # Change filePath according to local/github
+    fileName = "..\\bert_docs\\intermediate_postings"+str(docName)+".txt"
+    doc_content = ""
+
+    with open(fileName, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+        sections = content.split('\n\n\n')
+        count = 1
+        for section in sections:
+            if(count==i):
+                lines = section.split('\n')
+
+                if len(lines) >= 2:
+                    doc_content = ' '.join(lines[1:]).strip()  
+            count = count+1
+    return doc_content
+
+
+docid = 1001234
+content = fetchContentWithDocID(docid)
+print(content)
