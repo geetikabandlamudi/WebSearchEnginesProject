4d5903c6cfc38fe310c4fafb1bba18960928b468
geetikabandlamudi <42217232+geetikabandlamudi@users.noreply.github.com>
2023-12-24 17:50:00 -0500

    Update docContentManager.py

--- processing/docContentManager.py
+++ processing/docContentManager.py
@@ -1,24 +1,10 @@
-def fetchContentWithDocID(docid):
-    docName = int(docid/5000)+1
-    i = docid%5000
+import pickle
 
-    fileName = "./bert_docs/intermediate_postings"+str(docName)+".txt"
-    doc_content = ""
+class DocContentManager:
+    corpus = {}
+    def __init__(self):
+        with open('./embeddings/corpus', 'rb') as f:
+            self.corpus = pickle.load(f)
 
-    with open(fileName, 'r', encoding='utf-8') as file:
-        content = file.read()
-
-        sections = content.split('\n\n\n')
-        count = 1
-        for section in sections:
-            if(count==i):
-                lines = section.split('\n')
-
-                if len(lines) >= 2:
-                    doc_content = ' '.join(lines[1:]).strip()  
-            count = count+1
-    return doc_content
-
-# docid = 1001234
-# content = fetchContentWithDocID(docid)
-# print(content)
+    def fetchPassageContent(self, passageID):
+        return self.corpus[passageID]
